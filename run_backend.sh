9705bdefe4c527f78e9bc8e8835f51825a88ff35
dattp <tophatdat.160305@gmail.com>
2026-03-17 21:14:54 +0700

    resolve pull

--- run_backend.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export PYTORCH_CUDA_ALLOC_CONF='expandable_segments:True'
-export CUDA_VISIBLE_DEVICES=2
-
-PYTHONPATH=./backend uvicorn backend.main:app --reload --port 8000
--- tests/test_pipeline.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export PYTORCH_CUDA_ALLOC_CONF='expandable_segments:True'
-export CUDA_VISIBLE_DEVICES=2
-
-python ./source/tests/test_pipeline.py
